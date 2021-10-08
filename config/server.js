07683fccdd6bae2bfda0112e11a54ca0557363de
Calebe Tadeu <calebetadeu@gmail.com>
2021-10-08 15:21:54 -0300

    backend-list

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd0de94e662dbf3451e66e942b6fb7848'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b081a2e3-63fb-4f61-bdea-5552e6c46b4f'
+};
