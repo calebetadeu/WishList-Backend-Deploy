296624400c1988b0f2e964caa3afa7929049d3ff
Calebe Tadeu <calebetadeu@gmail.com>
2021-10-08 17:16:15 -0300

    profcfile

---
+++ admin/src/components/WysiwygWithErrors/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import MyNewWYSIWYG from 'my-awesome-lib';
+
+// This is a dummy example
+const WysiwygWithErrors = props => <MyNewWYSIWYG {...props} />;
+
+export default WysiwygWithErrors;
